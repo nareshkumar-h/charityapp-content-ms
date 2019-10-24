d461229f435078f4e0b763f52035d610183ca4aa
KatherinMaria04 <katherinmaria04@gmail.com>
2019-10-24 11:43:54 +0530

    first commit

---
+++ src/test/java/com/revature/charityappcontentms/CharityappContentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.charityappcontentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityappContentMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
