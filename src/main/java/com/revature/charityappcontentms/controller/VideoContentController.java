89ddc4bc924d00b3ec41eec1c336314478c8d337
KatherinMaria04 <katherinmaria04@gmail.com>
2019-10-25 10:44:19 +0530

    ADDED LIST CONTROLLER

--- src/main/java/com/revature/charityappcontentms/controller/VideoContentController.java
+++ src/main/java/com/revature/charityappcontentms/controller/VideoContentController.java
@@ -1,21 +1,27 @@
 package com.revature.charityappcontentms.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.charityapp.model.AdminAccess;
 import com.revature.charityappcontentms.dto.Message;
 import com.revature.charityappcontentms.dto.VideoContentDTO;
 import com.revature.charityappcontentms.exception.ServiceException;
-
+import com.revature.charityappcontentms.model.VideoContent;
 import com.revature.charityappcontentms.service.VideoContentService;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-
+@RestController
+@RequestMapping("videocontent")
 public class VideoContentController {
 
 	@Autowired
@@ -49,5 +55,28 @@ public class VideoContentController {
 		            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
 		        }
 	}
+	@GetMapping("/contentlist")
+	@ApiOperation(value = "  Content List API")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = VideoContent.class),
+            @ApiResponse(code = 400, message = "Invalid Credentials", response = Message.class) })
+	public ResponseEntity<?>listContent(){
+		
+		
+		List<VideoContent> videoContent = null;
+		String errorMessage = null;
+		try {
+			videoContent = videoContentService.contentList();
+		} catch (ServiceException e) {
+			errorMessage = e.getMessage();
+		}
+		
+		Message message = null;
+        if (  videoContent != null) {
+            return new ResponseEntity<>( videoContent, HttpStatus.OK);
+        } else {
+            message = new Message(errorMessage);
+            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        }
+    }
 	
 }
